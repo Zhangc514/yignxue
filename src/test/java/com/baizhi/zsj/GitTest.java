841fdd361a5fe8ef3954cefee76a3447c85ac57e
zhangce <903460595@qq.com>
2020-04-13 23:14:25 +0800

    添加遍历

--- src/test/java/com/baizhi/zsj/GitTest.java
+++ src/test/java/com/baizhi/zsj/GitTest.java
@@ -3,10 +3,14 @@ package com.baizhi.zsj;
 import org.junit.Test;
 
 public class GitTest {
-  @Test
+
+    @Test
   public void xixi(){
     int a=0;
     int b=1;
+        for (int i = 0; i < array.length; i++) {
+
+        }
   }
 }
 
