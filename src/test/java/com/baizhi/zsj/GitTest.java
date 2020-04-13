0b33979259013af5a1f2a600709384f8dd4fc85a
zhangce <903460595@qq.com>
2020-04-13 23:21:39 +0800

    删除之后添加

--- src/test/java/com/baizhi/zsj/GitTest.java
+++ src/test/java/com/baizhi/zsj/GitTest.java
@@ -1,17 +1,31 @@
 package com.baizhi.zsj;
 
-import org.junit.Test;
 
+import com.baizhi.zsj.entity.Emps;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class GitTest {
 
     @Test
-  public void xixi(){
-    int a=0;
-    int b=1;
-        for (int i = 0; i < array.length; i++) {
-
-        }
-  }
+    public void testPoi(){
+        
+    }
 }
-
-
